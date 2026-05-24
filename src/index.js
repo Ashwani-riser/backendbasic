3a42dd02e6f3fb3d2a4aa5c526bbd9e3a5676913
Ashwani Kumar <ashwani@example.com>
2026-05-24 13:19:52 +0530

    connect to mongoose atlas

---
+++ src/index.js
@@ -0,0 +1,32 @@
+//require("dotenv").config({path:"./.env"});
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+dotenv.config({ path: "./.env" });
+
+
+connectDB()
+
+
+
+
+
+
+
+// import express from "express";
+// (async () => {
+//     try {
+//          await mongoose.connect('${process.env.MONGODB_URI}/${DB_NAME}')
+//          app.on("error",(error) => {
+//             console.log("Error in connecting to database",error);
+//             throw error
+//          })
+
+//          app.listen(process.env.PORT,() => {
+//             console.log('App is listening on port ${process.env.PORT}' );
+//          })
+//     } catch(error){
+//         console.log("ERROR :",error)
+//         throw error
+//     }
+// })();
